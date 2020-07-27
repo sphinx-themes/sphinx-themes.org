c89211897f2f9f096e2e4a4b78b0cf37a7168e96
Pradyun Gedam <pradyunsg@users.noreply.github.com>
2020-07-27 11:25:42 +0530

    sort: Allow "pypi" to be null
    This is to accomodate for "default" themes

--- tools/sort-json.py
+++ tools/sort-json.py
@@ -1,13 +1,24 @@
 import json
 from pathlib import Path
 
-path = Path("themes.json")
 
-with path.open("r") as f:
-    data = json.load(f)
+def sort_key(theme):
+    one = theme["pypi"] or ""
+    two = theme["config"]
+    return one.lower(), two.lower()
 
-data["themes"].sort(key=lambda x: x["pypi"].lower())
 
-with path.open("w") as f:
-    json.dump(data, f, indent=2)
-    f.write("\n")
+def main():
+    path = Path("themes.json")
+
+    with path.open("r") as f:
+        data = json.load(f)
+
+    data["themes"].sort(key=sort_key)
+
+    with path.open("w") as f:
+        json.dump(data, f, indent=2)
+        f.write("\n")
+
+if __name__ == "__main__":
+    main()
