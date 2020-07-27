01e6d9ecbfa1e9cb851abace2278cd096aab2785
Pradyun Gedam <pradyunsg@users.noreply.github.com>
2020-07-27 17:20:22 +0530

    Allow for more powerful configuration setting
    Also lets me get rid of that annoying options key that wasn't being used.

--- tools/render-theme.py
+++ tools/render-theme.py
@@ -49,17 +49,62 @@ class Session:
 #
 # Actual logic
 #
+def _config_to_lines(config):
+    """
+
+    This is where there's a lot of complexity in this module. We allow
+    theme.config to either be a string (mapping directly to html_theme) or
+    to be a dictionary.
+
+    There are 3 special keys:
+    - _imports: a list of names, converted to "import name" statements.
+    - _extensions: a list of names, appended to "extensions" list.
+    - html_theme: name, automatically quoted in the final file.
+
+    The rest of the dictionary are transformed and added into the file as-is.
+    In other words, { "key": "value" } becomes a line "key = value" in the
+    conf.py file.
+    """
+    if isinstance(config, str):
+        config = {"html_theme": config}
+
+    # Convert config into lines.
+    config_lines = []
+
+    # Imports
+    imports = config.pop("_imports", [])
+    for module in imports:
+        config_lines.append(f"import {module}\n")
+
+    # Extensions
+    extensions = config.pop("_extensions", [])
+    for ext in extensions:
+        config_lines.append(f'extensions.append("{ext}")\n')
+
+    # Theme
+    html_theme = config.pop("html_theme")
+    assert html_theme is not None, "really need html_theme to be provided via config"
+    config_lines.append(f'html_theme = "{html_theme}"\n')
+
+    # Everything else
+    for key, value in config.items():
+        config_lines.append(f"{key} = {value}\n")
+
+    return config_lines
+
+
 def _patch_conf_file(theme):
+    """Patch the configuration file for this theme.
+    """
+    config_lines = _config_to_lines(theme.config)
+
+    # Actually manipulate the file.
     with CONF_PY_FILE.open() as f:
         lines = f.readlines()
 
-    for i, line in enumerate(lines[:]):
-        if line.startswith("html_theme = "):
-            line = f'html_theme = "{theme.config}"\n'
-        elif line.startswith("html_theme_options = "):
-            line = f"html_theme_options = {theme.options!r}\n"
-
-        lines[i] = line
+    # Find the marker we replace after.
+    index = lines.index("# !! MARKER !!\n")
+    lines[index+1:] = config_lines
 
     with CONF_PY_FILE.open("w") as f:
         f.writelines(lines)
