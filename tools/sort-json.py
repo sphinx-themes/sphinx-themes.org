cd7097e86fc674b96a3915f8ddba8db9f64ee51c
Pradyun Gedam <pradyunsg@users.noreply.github.com>
2020-09-15 16:05:22 +0530

    Add furo as a featured theme

--- tools/sort-json.py
+++ tools/sort-json.py
@@ -24,7 +24,7 @@ def sort_key(theme):
     # - featured
     # - third-party themes
     # - default themes
-    featured = ["alabaster", "sphinx-rtd-theme"]
+    featured = ["alabaster", "sphinx-rtd-theme", "furo"]
     if theme["pypi"] == "sphinx":
         rank = _get_index_or_default(
             featured, theme["config"], default=len(featured) + 1
