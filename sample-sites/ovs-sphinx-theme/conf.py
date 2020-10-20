3448ca570810ba7ffc8d1bf8dd8b27d44ffdc681
pradyunsg <pradyunsg@users.noreply.github.com>
2020-10-20 12:20:45 +0000

    deploy: 6d3b1bf91ae9923d5fcf8f01a1fc55fffdad6ee2

---
+++ sample-sites/ovs-sphinx-theme/conf.py
@@ -0,0 +1,30 @@
+import os
+import sys
+
+sys.path.append(os.path.join(os.environ["SAMPLE_DOCS_LOCATION"], "demo"))
+print("", sys.path[-1], "", sep="\n" + "-" * 80 + "\n")
+
+# -- Project information -----------------------------------------------------
+
+project = "Sphinx Themes Sample"
+copyright = "2020, Pradyun Gedam"
+author = "Pradyun Gedam"
+
+# -- Extensions --------------------------------------------------------------
+extensions = [
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.viewcode",
+]
+
+# -- Options for HTML output -------------------------------------------------
+
+html_title = project
+
+# NOTE: All the lines are after this are the theme-specific ones. These are
+#       written as part of the site generation pipeline for this project.
+# !! MARKER !!
+import ovs_sphinx_theme
+html_theme = "ovs"
+html_theme_path = [ovs_sphinx_theme.get_theme_dir()]
