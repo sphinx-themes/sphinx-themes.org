daf8a11ae05b212c494315f7a3f67b0386f239a8
pradyunsg <pradyunsg@users.noreply.github.com>
2020-10-21 06:04:31 +0000

    deploy: a2574dba48ef22181d4c05d89c131b783e5eacb2

---
+++ sample-sites/sphinx-sizzle-theme/app-data.js
@@ -0,0 +1,6 @@
+
+(function() {
+  var sizzle = $(document).data('sizzle');
+
+  sizzle.app_data = JSON.parse('{"glossary": {"document": "demo/demo", "terms": {"term-Documentation": {"term": "Documentation", "defn": "Provides users with the knowledge they need to use something."}, "term-Reading": {"term": "Reading", "defn": "The process of taking information into ones mind through the use of eyes."}, "term-Writing": {"term": "Writing", "defn": "The process of putting thoughts into a medium for other people to <a class=\\"reference internal\\" href=\\"#term-Reading\\"><span class=\\"xref std std-term\\">read</span></a>."}}}, "custom_data": {}}');
+})();
