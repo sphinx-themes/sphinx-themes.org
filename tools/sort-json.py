1b264e3c020c645b9fd3ec7886ded64c1e356b5e
Pradyun Gedam <pradyunsg@users.noreply.github.com>
2021-04-27 18:28:56 +0100

    Audit and clean up themes.json
    - Remove themes that were organisation-specific, project-specific or
      person-specific.
    - Remove "sphinx" and "theme" from the display names, since that is
      implied by being on a site called "sphinx-themes.org". :)
    - Remove themes that were minor modifications on sphinx-rtd-theme,
      and hadn't been updated in 6+ months.
    - Add more themes to the "featured" list, with the intention of
      showcasing the best-looking themes there. The order is arbitrary,
      based on what looked nice to me.

--- tools/sort-json.py
+++ tools/sort-json.py
@@ -1,6 +1,23 @@
 import json
 import re
+import sys
 from pathlib import Path
+
+# Only edit this if you're @pradyunsg or @shirou.
+_FEATURED = [
+    "alabaster",
+    "sphinx-rtd-theme",
+    "furo",
+    "sphinx-book-theme",
+    "pydata-sphinx-theme",
+    "sphinx-press-theme",
+    "sphinx-celery",
+    "insegel",
+    "insipid-sphinx-theme",
+    "sphinx-material",
+    "sphinx-redactor-theme",
+]
+
 
 _canonicalize_regex = re.compile(r"[-_.]+")
 
@@ -17,22 +34,47 @@ def _get_index_or_default(li, value, *, default):
 
 
 def sort_key(theme):
+    """Sort themes as "featured", "general", built-in."""
     # Normalize names
     theme["pypi"] = canonicalize_name(theme["pypi"])
 
-    # Determine the "rank" of a theme. Approximately ordered as:
-    # - featured
-    # - third-party themes
-    # - default themes
-    featured = ["alabaster", "classic", "furo", "sphinx-rtd-theme"]
     if theme["pypi"] == "sphinx":
         rank = _get_index_or_default(
-            featured, theme["config"], default=len(featured) + 1
+            _FEATURED,
+            theme["config"],
+            default=len(_FEATURED) + 1,
         )
     else:
-        rank = _get_index_or_default(featured, theme["pypi"], default=len(featured))
+        rank = _get_index_or_default(
+            _FEATURED,
+            theme["pypi"],
+            default=len(_FEATURED),
+        )
 
-    return (rank, theme["pypi"])
+    return (rank, theme["display"])
+
+
+def validate_display_names(data):
+    bad = set()
+    for theme in data["themes"]:
+        if theme["pypi"] == "sphinx":
+            continue
+
+        name = theme["display"].lower()
+        if "sphinx" in name or "theme" in name:
+            bad.add(f"display: {theme['display']}, pypi: {theme['pypi']}")
+
+    if bad:
+        bad_names = "\n".join(sorted(bad))
+        print(
+            f"FATAL: Got improper `display` values for packages:\n{bad_names}",
+            file=sys.stderr,
+        )
+        print(
+            "Don't include the words 'sphinx' and 'theme' in the display. "
+            'It is in a "Sphinx Themes Gallery" already.'
+        )
+        sys.exit(1)
 
 
 def main():
@@ -41,6 +83,7 @@ def main():
     with path.open("r") as f:
         data = json.load(f)
 
+    validate_display_names(data)
     data["themes"].sort(key=sort_key)
 
     with path.open("w") as f:
