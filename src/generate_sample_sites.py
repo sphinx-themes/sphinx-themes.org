f294ba563e04b1fd751aba575af730c7eebabf91
Pradyun Gedam <pradyunsg@users.noreply.github.com>
2022-09-29 00:42:07 +0100

    Use and mention the UTC timezone for date on the error page
    This makes it easier to figure out exactly *when* the build was
    performed.

--- src/generate_sample_sites.py
+++ src/generate_sample_sites.py
@@ -1,7 +1,7 @@
 import asyncio
 import os
 import shutil
-from datetime import datetime
+from datetime import datetime, timezone
 from pathlib import Path
 
 import rich.progress
@@ -18,7 +18,7 @@ def get_error_page(theme: Theme, error: Exception) -> str:
     return template.render(
         theme=theme,
         error=error,
-        now=datetime.now(),
+        now=datetime.now(tz=timezone.utc),
     )
 
 
