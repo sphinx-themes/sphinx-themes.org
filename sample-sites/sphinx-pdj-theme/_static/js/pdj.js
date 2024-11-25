6ef464b30845f793879071ef431d2f85ac32a85a
pradyunsg <pradyunsg@users.noreply.github.com>
2024-11-25 16:00:37 +0000

    deploy: 123b61b49a442f2389a260870aa2187d34ee69b5

--- sample-sites/sphinx-pdj-theme/_static/js/pdj.js
+++ sample-sites/sphinx-pdj-theme/_static/js/pdj.js
@@ -1,13 +1,25 @@
-// setting highlight for async stuff in python
-var async_kw = ['async', 'await'];
-jQuery.each(async_kw, function(i, kw){
-  var elements = jQuery(".highlight-python .n:contains('" + kw + "')");
-
-  jQuery.each(elements, function(j, el){
-    el = jQuery(el);
-    if (el.text() == kw){
-      el.removeClass('n');
-      el.addClass('kn');
+function setMenuControls() {
+  // Shift nav in mobile when clicking the menu.
+  let toggles = document.querySelectorAll("[data-toggle='wy-nav-top']");
+  for (let t of toggles) {
+    t.onclick = function(){
+      for (let e of document.querySelectorAll("[data-toggle='wy-nav-shift']")){
+	e.classList.toggle('shift');
+      }
     };
-  });
+  }
+
+  // Close menu when you click a link.
+  let links = document.querySelectorAll(".wy-menu-vertical .current ul li a");
+  for (let l of links) {
+    l.onclick = function(){
+      for (let e of document.querySelectorAll("[data-toggle='wy-nav-shift']")){
+	e.classList.toggle('shift');
+      }
+    };
+  }
+}
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  setMenuControls();
 });
